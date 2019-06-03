222133795b7fbbc4bf0a5928c3be1e5c68a18ef4
orvalho <rasa396@gmail.com>
2019-06-03 22:01:38 +0300

    fix - imgs don't show up in deployed app

--- src/components/UserAvatar.js
+++ src/components/UserAvatar.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default({userId}) => {
-  return <img className="ui avatar image" src={`/img/avatars/${userId}.png`} alt={`user-${userId}`}/>;
+  return <img className="ui avatar image" src={`./img/avatars/${userId}.png`} alt={`user-${userId}`}/>;
 };
